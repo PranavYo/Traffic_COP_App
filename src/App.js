64d38bcaa947eaee08629e05a20a722efba6c8cb
Ajit Singh <sajit8595@gmail.com>
2022-08-31 13:03:27 +0530

    modified, ajit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-// import logo from './logo.svg';
 import './App.css';
+import Login from './AjitComponents/Login'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <button className="btn btn-danger my-3">Convert to UpperCase</button>
+    <div>
+      <div className="App">
+        <button className="btn btn-danger my-3">Convert to UpperCase</button>
+      </div>
+      <Login/>
     </div>
   );
 }
