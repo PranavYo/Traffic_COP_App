0f78b9fb5cd44505957896e8885ea33d813bd6be
Abhishek Jha <abhishekkj522001@gmail.com>
2022-09-21 18:38:59 +0530

    Commited!!!

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import Dashboard from './InspectorComponents/Dashboard';
+import Dashboard from './MedicalComponents/Dashboard';
 
 function App() {
 
--- src/MedicalComponents/Dashboard.js
+++ src/MedicalComponents/Dashboard.js
@@ -1,3 +1,163 @@
-<script>
-        document.getElementById("demo").innerHTML = 10.50;
-</script>
+import React from 'react'
+
+export default function Dashboard() {
+  return (
+    <div>
+        <body>
+    
+    
+    <header class="header">
+    
+        <a href="#" class="logo"> <i class="fas fa-heartbeat"></i> Medicare </a>
+    
+        <nav class="navbar">
+            <a href="#home">home</a>
+            <a href="#services">services</a>
+            <a href="#about">about</a>
+        </nav>
+    
+        <div id="menu-btn" class="fas fa-bars"></div>
+    
+    </header>
+    
+
+    
+    <section class="home" id="home">
+    
+        <div class="image">
+            <img src="image/home-img.svg" alt=""/>
+        </div>
+    
+        <div class="content">
+            <h3>Stay Safe, Stay Healthy</h3>
+            <p>Our duty is to help members who are in emergency!!</p>
+            <a href="#" class="btn"> Contact s <span class="fas fa-chevron-right"></span> </a>
+        </div>
+    
+    </section>
+    
+
+    
+    <section class="icons-container">
+    
+        <div class="icons">
+            <i class="fas fa-user-md"></i>
+            <h3>140+</h3>
+            <p>doctors at work</p>
+        </div>
+    
+        <div class="icons">
+            <i class="fas fa-users"></i>
+            <h3>1040+</h3>
+            <p>satisfied patients</p>
+        </div>
+    
+        <div class="icons">
+            <i class="fas fa-procedures"></i>
+            <h3>500+</h3>
+            <p>bed facility</p>
+        </div>
+    
+        <div class="icons">
+            <i class="fas fa-hospital"></i>
+            <h3>80+</h3>
+            <p>available hospitals</p>
+        </div>
+    
+    </section>
+    
+    
+    <section class="services" id="services">
+    
+        <h1 class="heading"> our <span>services</span> </h1>
+    
+        <div class="box-container">
+    
+            <div class="box">
+                <i class="fas fa-notes-medical"></i>
+                <h3>free checkups</h3>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad, omnis.</p>
+                <a href="#" class="btn"> learn more <span class="fas fa-chevron-right"></span> </a>
+            </div>
+    
+            <div class="box">
+                <i class="fas fa-ambulance"></i>
+                <h3>24/7 ambulance</h3>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad, omnis.</p>
+                <a href="#" class="btn"> learn more <span class="fas fa-chevron-right"></span> </a>
+            </div>
+    
+            <div class="box">
+                <i class="fas fa-user-md"></i>
+                <h3>expert doctors</h3>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad, omnis.</p>
+                <a href="#" class="btn"> learn more <span class="fas fa-chevron-right"></span> </a>
+            </div>
+    
+            <div class="box">
+                <i class="fas fa-pills"></i>
+                <h3>medicines</h3>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad, omnis.</p>
+                <a href="#" class="btn"> learn more <span class="fas fa-chevron-right"></span> </a>
+            </div>
+    
+            <div class="box">
+                <i class="fas fa-procedures"></i>
+                <h3>bed facility</h3>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad, omnis.</p>
+                <a href="#" class="btn"> learn more <span class="fas fa-chevron-right"></span> </a>
+            </div>
+    
+            <div class="box">
+                <i class="fas fa-heartbeat"></i>
+                <h3>total care</h3>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad, omnis.</p>
+                <a href="#" class="btn"> learn more <span class="fas fa-chevron-right"></span> </a>
+            </div>
+    
+        </div>
+    
+    </section>
+    
+
+    
+    <section class="about" id="about">
+    
+        <h1 class="heading"> <span>about</span> us </h1>
+    
+        <div class="row">
+    
+            <div class="image">
+                <img src="image/about-img.svg" alt=""/>
+            </div>
+    
+            <div class="content">
+                <h3>we take care of your healthy life</h3>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure ducimus, quod ex cupiditate ullam in assumenda maiores et culpa odit tempora ipsam qui, quisquam quis facere iste fuga, minus nesciunt.</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus vero ipsam laborum porro voluptates voluptatibus a nihil temporibus deserunt vel?</p>
+                <a href="#" class="btn"> learn more <span class="fas fa-chevron-right"></span> </a>
+            </div>
+    
+        </div>
+    
+    </section>
+    
+    
+    </body>
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+  )
+}
+
