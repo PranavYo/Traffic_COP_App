24780ce085a02a56252351c03f880e3595e7f13f
Ajit Singh <sajit8595@gmail.com>
2022-09-21 18:44:24 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import Dashboard from './InspectorComponents/Dashboard';
 
+import DynamicTable from './TableCreation/DynamicTable';
+
 function App() {
 
   return (
     <div>
-        <Dashboard/>
+       {/*  <Dashboard/> */}
+        <DynamicTable/>
     </div>
   );
 }
